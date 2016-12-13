ecb0e333012d1e60ea34c61f0c8a048e7739d5c7
Luke1982 <gcx3@hotmail.com>
2016-12-13 20:14:48 +0100

    Creates a new servicejob for each selected asset on salesorder save
    Also relates the selected assets to the salesorder

--- modules/ServiceJob/resources/handlers/onSalesOrderSave.php
+++ modules/ServiceJob/resources/handlers/onSalesOrderSave.php
@@ -2,16 +2,36 @@
 
 Class SoSaveHandler extends VTEventHandler {
 	public function handleEvent($eventName, $entityData){
+		global $current_user, $adb;
+
 		$moduleName = $entityData->getModuleName();
 		if ($moduleName == 'SalesOrder') {
-			$soId = $entityData->getId();
-			
-			echo "<pre>";
-			print_r($_REQUEST['hdn_asset']);
-			print_r($soId);
-			echo "</pre>";
 
-			die();
+			$soId = $entityData->getId();
+			$soData = $entityData->getData();
+
+			require_once('modules/ServiceJob/ServiceJob.php');
+			
+			foreach ($_REQUEST['hdn_asset'] as $k => $v) {
+				if ($v != '') {
+
+					$sj_focus = new ServiceJob();
+					$sj_focus->column_fields['assigned_user_id'] = $soData['assigned_user_id'];
+					$sj_focus->column_fields['execution_date'] = $soData['duedate'];
+					$sj_focus->column_fields['related_asset_id'] = $v;
+
+			 		$handler = vtws_getModuleHandlerFromName('ServiceJob', $current_user); 
+			 		$meta = $handler->getMeta();
+			 		$sj_focus->column_fields = DataTransform::sanitizeRetrieveEntityInfo($sj_focus->column_fields, $meta);
+
+			 		$sj_focus->save('ServiceJob');
+
+			 		$adb->pquery("INSERT INTO vtiger_crmentityrel (crmid, module, relcrmid, relmodule) VALUES (?,?,?,?)", array($soId, 'SalesOrder', $sj_focus->id, 'ServiceJob'));
+			 		$adb->pquery("INSERT INTO vtiger_crmentityrel (crmid, module, relcrmid, relmodule) VALUES (?,?,?,?)", array($soId, 'SalesOrder', $v, 'Assets'));
+
+				}
+			}
+
 		}
 	}
 }
