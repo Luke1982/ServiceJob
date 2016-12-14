35001880f8382d34d06f12de3ee5c054c49cd5af
Luke1982 <gcx3@hotmail.com>
2016-12-14 18:22:35 +0100

    Start handling the cllsing of the service job

--- modules/ServiceJob/resources/handlers/closeServiceJob.php
+++ modules/ServiceJob/resources/handlers/closeServiceJob.php
@@ -1,5 +1,23 @@
 <?php
 
 function closeServiceJob($id, $values, $user) {
-	return $id;
+
+	require_once('modules/ServiceJob/ServiceJob.php');
+
+	list($sj_wsid, $sj_id) = explode('x', $id);
+	$sj = new ServiceJob();
+	$sj->retrieve_entity_info($sj_id, 'ServiceJob');
+	$sj->id = $sj_id;
+	$sj->mode = 'edit';
+
+	$sj->column_fields['servicejob_status'] = 'Completed';
+	$sj->column_fields['mechanic_remarks'] = $values['opmerkingen_monteur'];
+
+	$handler = vtws_getModuleHandlerFromName('ServiceJob', $user);
+	$meta = $handler->getMeta();
+	$sj->column_fields = DataTransform::sanitizeRetrieveEntityInfo($sj->column_fields, $meta);
+
+	$sj->save('ServiceJob');
+	// return $id;
+	return true;
 }
