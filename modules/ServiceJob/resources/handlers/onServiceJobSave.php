2ed77ee1f1115f8cd2ae605deaca7ae46f3fbcb0
Luke1982 <gcx3@hotmail.com>
2017-04-24 17:02:34 +0200

    fix: check for exact relation between asset and servicejob
    Not just for asset ID.

--- modules/ServiceJob/resources/handlers/onServiceJobSave.php
+++ modules/ServiceJob/resources/handlers/onServiceJobSave.php
@@ -13,7 +13,7 @@ Class SJSaveHandler extends VTEventHandler {
 			// Relate this servicejob the the related asset in the asset's related list.
 			if ($sjData['related_asset_id'] != null) {
 			// First see if the relation was already set
-				$r = $adb->pquery("SELECT * FROM vtiger_crmentityrel WHERE relcrmid = ?", array($sjData['related_asset_id']));
+				$r = $adb->pquery("SELECT * FROM vtiger_crmentityrel WHERE relcrmid = ? AND crmid = ?", array($sjData['related_asset_id'], $sjId));
 				// Only perform operation if row count is zero (relation not yet set)
 				if ($adb->num_rows($r) == 0) {
 					$adb->pquery("INSERT INTO vtiger_crmentityrel (crmid, module, relcrmid, relmodule) VALUES (?,?,?,?)", array($sjData['related_asset_id'], 'Assets', $sjId, 'ServiceJob'));
