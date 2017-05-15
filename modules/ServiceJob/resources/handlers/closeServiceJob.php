018a6280a25f5e1d5cba571c3bb02bb23fc5566f
Luke1982 <gcx3@hotmail.com>
2017-05-15 13:37:24 +0200

    Handle the cancelling of incoming servicejobs from the portal

--- modules/ServiceJob/resources/handlers/closeServiceJob.php
+++ modules/ServiceJob/resources/handlers/closeServiceJob.php
@@ -5,60 +5,85 @@
 
 function closeServiceJob($id, $values, $user) {
 
-	require_once('modules/ServiceJob/ServiceJob.php');
-	require_once('modules/Assets/Assets.php');
-	require_once('modules/ServiceJob/resources/classes/ServiceJobReport.php');
-	require_once('Smarty_setup.php');
+	if ($values['action'] != 'cancel') {
+		
+		require_once('modules/ServiceJob/ServiceJob.php');
+		require_once('modules/Assets/Assets.php');
+		require_once('modules/ServiceJob/resources/classes/ServiceJobReport.php');
+		require_once('Smarty_setup.php');
 
-	list($sj_wsid, $sj_id) = explode('x', $id);
-	$sj = new ServiceJob();
-	$sj->retrieve_entity_info($sj_id, 'ServiceJob');
-	$sj->id = $sj_id;
-	$sj->mode = 'edit';
+		list($sj_wsid, $sj_id) = explode('x', $id);
+		$sj = new ServiceJob();
+		$sj->retrieve_entity_info($sj_id, 'ServiceJob');
+		$sj->id = $sj_id;
+		$sj->mode = 'edit';
 
-	$sj->column_fields['servicejob_status'] = 'Completed';
-	$sj->column_fields['mechanic_remarks'] = $values['mechanic_remarks'];
-	$sj->column_fields['assigned_user_id'] = $user->id;
+		$sj->column_fields['servicejob_status'] = 'Completed';
+		$sj->column_fields['mechanic_remarks'] = $values['mechanic_remarks'];
+		$sj->column_fields['assigned_user_id'] = $user->id;
 
-	$handler = vtws_getModuleHandlerFromName('ServiceJob', $user);
-	$meta = $handler->getMeta();
-	$sj->column_fields = DataTransform::sanitizeRetrieveEntityInfo($sj->column_fields, $meta);
-
-	$sj->save('ServiceJob');
-
-	if (array_key_exists('new_asset_expirydate', $values))
-		$rel_asset_id = $sj->column_fields['related_asset_id'];
-		$ass = new Assets();
-		$ass->retrieve_entity_info($rel_asset_id, 'Assets');
-		$ass->id = $rel_asset_id;
-		$ass->mode = 'edit';
-
-		$ass->column_fields['cf_731'] = $values['new_asset_expirydate'];
-		$ass->column_fields['cf_966'] = 'Gekeurd';
-		$handler = vtws_getModuleHandlerFromName('Assets', $user);
+		$handler = vtws_getModuleHandlerFromName('ServiceJob', $user);
 		$meta = $handler->getMeta();
-		$ass->column_fields = DataTransform::sanitizeRetrieveEntityInfo($ass->column_fields, $meta);
+		$sj->column_fields = DataTransform::sanitizeRetrieveEntityInfo($sj->column_fields, $meta);
 
-		$ass->save('Assets');
+		$sj->save('ServiceJob');
+
+		if (array_key_exists('new_asset_expirydate', $values)) {
+			$rel_asset_id = $sj->column_fields['related_asset_id'];
+			$ass = new Assets();
+			$ass->retrieve_entity_info($rel_asset_id, 'Assets');
+			$ass->id = $rel_asset_id;
+			$ass->mode = 'edit';
+
+			$ass->column_fields['cf_731'] = $values['new_asset_expirydate'];
+			$ass->column_fields['cf_966'] = 'Gekeurd';
+			$handler = vtws_getModuleHandlerFromName('Assets', $user);
+			$meta = $handler->getMeta();
+			$ass->column_fields = DataTransform::sanitizeRetrieveEntityInfo($ass->column_fields, $meta);
+
+			$ass->save('Assets');
+		}
+		
+		$report = new ServiceJobReport();
+		$smarty = new vtigerCRM_Smarty();
+
+		$report_metadata = $report->getMeta($sj_id);
+		$report_metadata['user'] = $user->column_fields;
+		$report_metadata['company'] = $report->getCompany();
+
+		$date = new DateTime();
+		$report_metadata['date'] = $date->format('d-m-Y');
+
+		$report_path = 'modules/ServiceJob/reporttemplates/report_'.$values['servicejob_procedure'].'.tpl';
+
+		$smarty->assign('reportmeta', $report_metadata);
+		$smarty->assign('reportvalues', $values);
+		$htmlreport = $smarty->fetch($report_path);
+
+		$report->save($htmlreport, $sj_id);
+
+	} else if ($values['action'] == 'cancel') {
+
+		// If a servicejob is cancelled from the portal
+		require_once('modules/ServiceJob/ServiceJob.php');
+
+		list($sj_wsid, $sj_id) = explode('x', $id);
+		$sj = new ServiceJob();
+		$sj->retrieve_entity_info($sj_id, 'ServiceJob');
+		$sj->id = $sj_id;
+		$sj->mode = 'edit';
+
+		$sj->column_fields['servicejob_status'] = 'Cancelled';
+		$sj->column_fields['mechanic_remarks'] = $values['cancel_reason'];
+		$sj->column_fields['assigned_user_id'] = $user->id;
+
+		$handler = vtws_getModuleHandlerFromName('ServiceJob', $user);
+		$meta = $handler->getMeta();
+		$sj->column_fields = DataTransform::sanitizeRetrieveEntityInfo($sj->column_fields, $meta);
+
+		$sj->save('ServiceJob');
+
 	}
-	
-	$report = new ServiceJobReport();
-	$smarty = new vtigerCRM_Smarty();
-
-	$report_metadata = $report->getMeta($sj_id);
-	$report_metadata['user'] = $user->column_fields;
-	$report_metadata['company'] = $report->getCompany();
-
-	$date = new DateTime();
-	$report_metadata['date'] = $date->format('d-m-Y');
-
-	$report_path = 'modules/ServiceJob/reporttemplates/report_'.$values['servicejob_procedure'].'.tpl';
-
-	$smarty->assign('reportmeta', $report_metadata);
-	$smarty->assign('reportvalues', $values);
-	$htmlreport = $smarty->fetch($report_path);
-
-	$report->save($htmlreport, $sj_id);
 
 	return true;
 }
