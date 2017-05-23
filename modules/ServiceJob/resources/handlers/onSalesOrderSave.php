cf7dba62f8201bc8e6a33f31f159796a6d98dc3c
Guido <guido@Tims-MacBook-Pro.local>
2017-05-23 21:31:24 +0200

    Use new serial field in SalesOrder save handler

--- modules/ServiceJob/resources/handlers/onSalesOrderSave.php
+++ modules/ServiceJob/resources/handlers/onSalesOrderSave.php
@@ -16,12 +16,26 @@ Class SoSaveHandler extends VTEventHandler {
 			foreach ($_REQUEST['hdn_asset'] as $k => $v) {
 				if ($k != '') {
 
+			 		// Update the asset "keurstatus"
+					$ass = new Assets();
+					$ass->retrieve_entity_info($k, 'Assets');
+					$ass->id = $k;
+					$ass->mode = 'edit';
+					$ass->column_fields['cf_966'] = 'Keuring ingepland'; // Adjust custom field ID
+					$ass_serial = $ass->column_fields['serialnumber']; // Get serial no to add to SJ later
+
+					$handler = vtws_getModuleHandlerFromName('Assets', $current_user);
+					$meta = $handler->getMeta();
+					$ass->column_fields = DataTransform::sanitizeRetrieveEntityInfo($ass->column_fields, $meta);
+					$ass->save('Assets');
+
 					$sj_focus = new ServiceJob();
 					$sj_focus->column_fields['assigned_user_id'] = $soData['reports_to_id'];
 					$sj_focus->column_fields['execution_date'] = $soData['duedate'];
 					$sj_focus->column_fields['related_asset_id'] = $k;
 					$sj_focus->column_fields['related_so_id'] = $soId;
 					$sj_focus->column_fields['servicejob_status'] = 'Planned';
+					$sj_focus->column_fields['servicejob_for_serial'] = $ass_serial;
 					$sj_focus->column_fields['servicejob_productname'] = trim($v);
 
 			 		$handler = vtws_getModuleHandlerFromName('ServiceJob', $current_user); 
@@ -33,16 +47,6 @@ Class SoSaveHandler extends VTEventHandler {
 			 		$adb->pquery("INSERT INTO vtiger_crmentityrel (crmid, module, relcrmid, relmodule) VALUES (?,?,?,?)", array($soId, 'SalesOrder', $sj_focus->id, 'ServiceJob'));
 			 		$adb->pquery("INSERT INTO vtiger_crmentityrel (crmid, module, relcrmid, relmodule) VALUES (?,?,?,?)", array($soId, 'SalesOrder', $k, 'Assets'));
 
-			 		// Update the asset "keurstatus"
-					$ass = new Assets();
-					$ass->retrieve_entity_info($k, 'Assets');
-					$ass->id = $k;
-					$ass->mode = 'edit';
-					$ass->column_fields['cf_966'] = 'Keuring ingepland'; // Adjust custom field ID
-					$handler = vtws_getModuleHandlerFromName('Assets', $current_user);
-					$meta = $handler->getMeta();
-					$ass->column_fields = DataTransform::sanitizeRetrieveEntityInfo($ass->column_fields, $meta);
-					$ass->save('Assets');
 				}
 			}
 
