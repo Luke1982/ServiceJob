52e71e35ae74897804c46882b40464cc4a0acfda
Luke1982 <gcx3@hotmail.com>
2018-01-17 11:12:07 +0100

    Don't return assets that are not in use in salesorder autocomplete

--- modules/ServiceJob/returnAssetsForAccount.php
+++ modules/ServiceJob/returnAssetsForAccount.php
@@ -10,7 +10,7 @@ if ($_REQUEST['selected'] != '') {
 	$selected_array = explode(',', $_REQUEST['selected']);
 }
 
-$r = $adb->pquery("SELECT vtiger_assets.serialnumber, vtiger_assets.assetname, vtiger_assets.assetstatus, vtiger_assets.assetsid, vtiger_products.productname FROM vtiger_assets INNER JOIN vtiger_products ON vtiger_assets.product = vtiger_products.productid INNER JOIN vtiger_crmentity ON vtiger_assets.assetsid = vtiger_crmentity.crmid WHERE vtiger_assets.account = ? AND vtiger_crmentity.deleted = ?", array($acc_id, 0));
+$r = $adb->pquery("SELECT vtiger_assets.serialnumber, vtiger_assets.assetname, vtiger_assets.assetstatus, vtiger_assets.assetsid, vtiger_products.productname FROM vtiger_assets INNER JOIN vtiger_products ON vtiger_assets.product = vtiger_products.productid INNER JOIN vtiger_crmentity ON vtiger_assets.assetsid = vtiger_crmentity.crmid WHERE vtiger_assets.account = ? AND vtiger_assets.assetstatus = ? AND vtiger_crmentity.deleted = ?", array($acc_id, 'In Gebruik', 0));
 
 while ($row = $adb->fetch_array($r)) {
 	$tmp = [];
