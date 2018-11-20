1c6b6b27812a9e627fa608d197399194dbfae9d8
Luke1982 <gcx3@hotmail.com>
2018-11-20 10:50:59 +0100

    Improve presence of buildyear when updating an asset through webservice

--- modules/ServiceJob/resources/handlers/closeServiceJob.php
+++ modules/ServiceJob/resources/handlers/closeServiceJob.php
@@ -53,7 +53,7 @@ function closeServiceJob($id, $values, $user) {
 			}
 			$ass->column_fields['cf_731'] = $values['new_asset_expirydate'];
 			$ass->column_fields['cf_966'] = 'Gekeurd';
-			$ass->column_fields['cf_903'] = $values['buildyear'];
+			$ass->column_fields['cf_903'] = isset($values['buildyear']) ? $values['buildyear'] : date('Y');
 			$handler = vtws_getModuleHandlerFromName('Assets', $user);
 			$meta = $handler->getMeta();
 			$ass->column_fields = DataTransform::sanitizeRetrieveEntityInfo($ass->column_fields, $meta);
