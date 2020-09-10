9786c47f476875fe8af76aa01deb415dfce4b3d9
Luke1982 <gcx3@hotmail.com>
2020-09-10 09:28:55 +0200

    Prevent errors when no assets are added to the salesorder

--- modules/ServiceJob/resources/handlers/onSalesOrderSave.php
+++ modules/ServiceJob/resources/handlers/onSalesOrderSave.php
@@ -12,30 +12,32 @@ class SoSaveHandler extends VTEventHandler {
 			require_once 'modules/ServiceJob/ServiceJob.php';
 			require_once 'modules/Assets/Assets.php';
 
-			foreach ($_REQUEST['hdn_asset'] as $k => $v) {
-				if ($k != '') {
-					// Update the asset "keurstatus"
-					$ass = new Assets();
-					$ass->retrieve_entity_info($k, 'Assets');
-					$ass_serial = $ass->column_fields['serialnumber']; // Get serial no to add to SJ later
+			if (array_key_exists('hdn_asset', $_REQUEST)) {
+				foreach ($_REQUEST['hdn_asset'] as $k => $v) {
+					if ($k != '') {
+						// Update the asset "keurstatus"
+						$ass = new Assets();
+						$ass->retrieve_entity_info($k, 'Assets');
+						$ass_serial = $ass->column_fields['serialnumber']; // Get serial no to add to SJ later
 
-					$sj_focus = new ServiceJob();
-					$sj_focus->column_fields['assigned_user_id'] = $soData['reports_to_id'];
-					$sj_focus->column_fields['execution_date'] = $soData['duedate'];
-					$sj_focus->column_fields['related_asset_id'] = $k;
-					$sj_focus->column_fields['related_so_id'] = $soId;
-					$sj_focus->column_fields['servicejob_status'] = 'Planned';
-					$sj_focus->column_fields['servicejob_for_serial'] = $ass_serial;
-					$sj_focus->column_fields['servicejob_productname'] = trim($v);
+						$sj_focus = new ServiceJob();
+						$sj_focus->column_fields['assigned_user_id'] = $soData['reports_to_id'];
+						$sj_focus->column_fields['execution_date'] = $soData['duedate'];
+						$sj_focus->column_fields['related_asset_id'] = $k;
+						$sj_focus->column_fields['related_so_id'] = $soId;
+						$sj_focus->column_fields['servicejob_status'] = 'Planned';
+						$sj_focus->column_fields['servicejob_for_serial'] = $ass_serial;
+						$sj_focus->column_fields['servicejob_productname'] = trim($v);
 
-					$handler = vtws_getModuleHandlerFromName('ServiceJob', $current_user);
-					$meta = $handler->getMeta();
-					$sj_focus->column_fields = DataTransform::sanitizeRetrieveEntityInfo($sj_focus->column_fields, $meta);
+						$handler = vtws_getModuleHandlerFromName('ServiceJob', $current_user);
+						$meta = $handler->getMeta();
+						$sj_focus->column_fields = DataTransform::sanitizeRetrieveEntityInfo($sj_focus->column_fields, $meta);
 
-					$sj_focus->save('ServiceJob');
+						$sj_focus->save('ServiceJob');
 
-					$adb->pquery("INSERT INTO vtiger_crmentityrel (crmid, module, relcrmid, relmodule) VALUES (?,?,?,?)", array($soId, 'SalesOrder', $sj_focus->id, 'ServiceJob'));
-					$adb->pquery("INSERT INTO vtiger_crmentityrel (crmid, module, relcrmid, relmodule) VALUES (?,?,?,?)", array($soId, 'SalesOrder', $k, 'Assets'));
+						$adb->pquery("INSERT INTO vtiger_crmentityrel (crmid, module, relcrmid, relmodule) VALUES (?,?,?,?)", array($soId, 'SalesOrder', $sj_focus->id, 'ServiceJob'));
+						$adb->pquery("INSERT INTO vtiger_crmentityrel (crmid, module, relcrmid, relmodule) VALUES (?,?,?,?)", array($soId, 'SalesOrder', $k, 'Assets'));
+					}
 				}
 			}
 		}
